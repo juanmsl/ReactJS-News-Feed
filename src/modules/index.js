9548d8155ace8434dbcc30bca606fd94e7086549
Juan Manuel Sánchez Lozano <juanmsl_pk@hotmail.com>
2019-11-08 21:26:09 -0500

    basename

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Switch, Route} from "react-router-dom";
+import {BrowserRouter, Switch, Route, Redirect} from "react-router-dom";
 import Home from './home';
 import Category from './category';
 import Search from './search';
@@ -7,12 +7,15 @@ import Search from './search';
 
 export default class NewsFeedApp extends React.Component {
   render() {
+    const basename = process.env.NODE_ENV === "production" ? "/ReactJS-News-Feed" : "";
+
     return (
-      <BrowserRouter>
+      <BrowserRouter basename={basename}>
         <Switch>
           <Route exact path="/" component={Home}/>
           <Route exact path="/category/:categoryName" component={Category}/>
           <Route exact path="/search/:searchValue" component={Search}/>
+          <Redirect to="/" />
         </Switch>
       </BrowserRouter>
     );
